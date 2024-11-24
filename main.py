56c2374733a992164b91f69237dd2d323533ebf6
Craig Martin <craigmichaelmartin@users.noreply.github.com>
2024-11-24 18:42:40 +0000

    use curses by default

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from game import Game
-# from ui.curses_graphics import Graphics
+from ui.curses_graphics import Graphics
 
 # We use the curses engine due to the school's WiFi restricting
 # ports that Replit's VNC server requires to run Pygame.
 # If you are running this at home, comment-out line 1 and
 # un-comment-out the line below this so that the game uses Pygame.
-from ui.pygame_graphics import Graphics
+# from ui.pygame_graphics import Graphics
 
 
 def on_ready(graphics):
